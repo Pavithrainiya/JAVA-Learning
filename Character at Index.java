e6f93d006e64a6a002d6744a65e826c7b4ad2ff1
Pavithra <pavijeevi56@gmail.com>
2024-10-16 14:57:22 +0530

    Added Character at Index.java

---
+++ Character at Index.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s="mohammed samiqkhan,frd of madhu";
+        //int len=s.length();
+        //System.out.print(len);
+        char charindex=s.charAt(5);
+        System.out.print(charindex);
+    }
+}
