df490a62097b12681342db8770cd6bc9bb9e4b9b
Pavithra <pavijeevi56@gmail.com>
2024-10-16 14:54:15 +0530

    Added ExtractSubstringFromIndex.java

---
+++ ExtractSubstringFromIndex.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s="Sowburnika frnd of pavithra";
+        //int len=s.length();
+        //System.out.print(len);
+        String substring=s.substring(15);
+        System.out.print(substring);
+    }
+}
