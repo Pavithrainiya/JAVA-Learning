fe7ca2632a206e636216a598e35d4b25ebdf4b39
Pavithra <pavijeevi56@gmail.com>
2024-10-16 14:55:48 +0530

    Added CharacterAtIndex.java

---
+++ CharacterAtIndex.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s="Sowburnika frnd of pavithra";
+        //int len=s.length();
+        //System.out.print(len);
+        char charindex=s.charAt(16);
+        System.out.print(charindex);
+    }
+}
